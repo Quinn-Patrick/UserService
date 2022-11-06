046b23264e2581c28dda49e5316cb40ff9099bb4
Quinn Bierbaum <quinn.bierbaum@gmail.com>
2022-11-06 10:04:40 -0500

    Trying database url as a string literal.

--- src/main/java/com/quinnsgames/userservice/config/JpaConfig.java
+++ src/main/java/com/quinnsgames/userservice/config/JpaConfig.java
@@ -4,15 +4,21 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.quinnsgames.userservice.api.MessageResource;
+
+import lombok.extern.slf4j.Slf4j;
+
 import javax.sql.DataSource;
 
 @Configuration
+@Slf4j
 public class JpaConfig {
 	@Bean
     public DataSource getDataSource() {
+		log.info(System.getenv("MYSQL_DB_URL"));
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url(System.getenv("MYSQL_DB_URL"))
+                .url(System.getenv("jdbc:mysql://quinns-flexible-db-server.mysql.database.azure.com:3306/dwr_monster_api"))
                 .username(System.getenv("MYSQL_DB_USER"))
                 .password(System.getenv("MYSQL_DB_ROOT_PASSWORD"))
                 .build();
