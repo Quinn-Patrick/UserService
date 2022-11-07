0121ff072a2a5a0e871a1e745b295bc3188949ff
Quinn Bierbaum <quinn.bierbaum@gmail.com>
2022-11-06 22:04:38 -0500

    Changed to using the spring enviroment class to get environment variables, in hopes of fixing deployment.

--- src/main/java/com/quinnsgames/userservice/config/JpaConfig.java
+++ src/main/java/com/quinnsgames/userservice/config/JpaConfig.java
@@ -1,29 +1,32 @@
 package com.quinnsgames.userservice.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.quinnsgames.userservice.api.MessageResource;
+import org.springframework.core.env.Environment;
 
 import lombok.extern.slf4j.Slf4j;
-
-import javax.sql.DataSource;
 
 @Configuration
 @Slf4j
 public class JpaConfig {
+	@Autowired
+	private Environment environment;
+	
 	@Bean
     public DataSource getDataSource() {
-		log.info(System.getenv("MYSQL_DB_URL"));
-		log.info(System.getenv("APPDATA"));
-		log.info(System.getenv("COMPUTERNAME"));
+		log.info(environment.getProperty("MYSQL_DB_URL"));
+		log.info(System.getProperty("APPDATA"));
+		log.info(System.getProperty("COMPUTERNAME"));
 		log.info(System.getenv("NUM_CORES"));
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.cj.jdbc.Driver")
                 .url("jdbc:mysql://quinns-flexible-db-server.mysql.database.azure.com:3306/dwr_monster_api")
-                .username(System.getenv("MYSQL_DB_USER"))
-                .password(System.getenv("MYSQL_DB_ROOT_PASSWORD"))
+                .username(environment.getProperty("MYSQL_DB_USER"))
+                .password(environment.getProperty("MYSQL_DB_ROOT_PASSWORD"))
                 .build();
     }
 }
