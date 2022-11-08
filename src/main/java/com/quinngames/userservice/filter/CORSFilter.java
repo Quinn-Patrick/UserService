c964ff03d06426e0f74a8490d92362e8019e4517
Quinn Bierbaum <quinn.bierbaum@gmail.com>
2022-11-07 21:38:56 -0500

    Allowed origin should now take anything

--- src/main/java/com/quinngames/userservice/filter/CORSFilter.java
+++ src/main/java/com/quinngames/userservice/filter/CORSFilter.java
@@ -38,7 +38,7 @@ public class CORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         String origin = request.getHeader("Origin");
         
-        response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "*");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Vary", "Origin");
